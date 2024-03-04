798d823ec49aa47716dab246cedafc6ae365a886
Sheryl L <s_lawrence0@yahoo.ca>
2024-03-03 21:19:56 -0500

    consts added to index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // TODO: Include packages needed for this application
-
+// npm package 
+const inquirer  = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+console.log("This is my command-line application to generate a README!");
+console.log("The following questions were answered to generate a high-quality README.");
 // TODO: Create an array of questions for user input
 const questions = [];
 
