bf3b4eca356c81acb18059dd67f4b899a06edcef
Sheryl L <s_lawrence0@yahoo.ca>
2024-03-03 22:07:15 -0500

    const questions array added to index.js

--- index.js
+++ index.js
@@ -6,7 +6,43 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 console.log("This is my command-line application to generate a README!");
 console.log("The following questions were answered to generate a high-quality README.");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of this project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a description of the project",
+    },
+    {
+        type: "input",
+        name: "table of contents",
+        message: "List the contents of this project.",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What is the title of this project?",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Procide instructions for use of this project.",
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "List the contributors of this project.",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "test",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
